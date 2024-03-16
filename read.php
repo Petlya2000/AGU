61ad97c869de5f460b638155309912b89fbf3938
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-03-16 22:29:45 +0300

    Update read.php

--- read.php
+++ read.php
@@ -17,7 +17,7 @@ else {
 $sql_select = "SELECT * FROM ankety INNER JOIN spr_region ON ankety.reg_id = spr_region.id_region INNER JOIN gender ON ankety.gender_id = gender.gend_id 
 INNER JOIN languages ON ankety.language_id = languages.language_id INNER JOIN nationality ON ankety.nationality_id = nationality.nation_id
 INNER JOIN countries ON ankety.country_id = countries.country_id
-INNER JOIN uch_zav ON ankety.uch_zav_id = uch_zav.uch_zav_id INNER JOIN vid_mili_serv ON ankety.vid_mili_serv_id = vid_mili_serv.vid_mili_serv_id WHERE Ankety_id='$id'";
+INNER JOIN uch_zav ON ankety.uch_zav_id = uch_zav.uch_zav_id INNER JOIN vid_mili_serv ON ankety.vid_milit_serv_id = vid_mili_serv.vid_milit_serv_id WHERE Ankety_id='$id'";
 $result = mysqli_query($con,$sql_select);
 $row = mysqli_fetch_array($result);
 if($row) { printf("<p><b>Номер студента: ".$row['Ankety_id']."</b></p><p><b>Фамилия : ".$row['Ankety_fam']."</b></p><p><b>Имя : ".$row['Ankety_name1']."</b></p><p><b>Отчество : ".$row['Ankety_otch']."</b></p><p><b>Пол : ".$row['gend_name']."</b></p><p><b>Паспорт: ".$row['document']."</b></p><p><b>Дата рождения: ".$row['date_birth']
