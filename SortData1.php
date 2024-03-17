e6b91308efaeb5aeb48fbc3edf13f4576d975671
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-03-18 01:31:08 +0300

    Update SortData1.php

--- SortData1.php
+++ SortData1.php
@@ -1,23 +1,33 @@
 <?php
-    require_once 'conn.php';
+require_once 'connect.php';
 $data = $_REQUEST['nk'];
 if (!($data)) {
-  echo("Введите необходимую дату");
+  echo("Введите автора книги");
 }
 else {
-  $sql_select="SELECT * FROM ankety INNER JOIN spr_region ON ankety.reg_id = spr_region.id_region INNER JOIN gender ON ankety.gender_id = gender.gend_id INNER JOIN nationality ON ankety.nationality_id = nationality.nation_id WHERE ankety.date_birth >'$data' ORDER BY ankety.Ankety_id ASC;";
-  $result=mysqli_query($con,$sql_select);
+   $sql_select="SELECT * FROM ankety INNER JOIN spr_region ON ankety.reg_id = spr_region.id_region INNER JOIN gender ON ankety.gender_id = gender.gend_id INNER JOIN nationality ON ankety.nationality_id = nationality.nation_id WHERE ankety.date_birth > '$data'ORDER BY ankety.Ankety_id ASC;";
+$result = mysqli_query($conn,$sql_select);
+$row = mysqli_fetch_array($result);
+if(mysqli_num_rows($result)==0){
+  echo("Таких книг нет");
+  else {
+  do 
+  {
+printf($row['id']); printf("<br/>");printf($row['author']); printf("<br/>"); printf($row['title']);printf("<br/>"); printf($row['text']); }
+}
+  while($row = mysqli_fetch_array($result));
+}
 ?>
-  
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 <head>
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html">
-<title>Кратко о студентах</title></head>
+<title>Поиск по Дате</title>
+</head>
 <body>
-    <h1>Краткие данные о студентах</h1>
-     <br />  
+    <h1>Сортировка по дате</h1>
+    <br />  
            <div class="container" style="width:500px;">  
                 <div class="table-responsive">  
                      <table class="table table-striped">  
@@ -32,8 +42,9 @@ else {
                               <th> Национальность</th>
                            </tr>  
                           <?php  
-                          if(mysqli_num_rows($result) > 0)  
-                          {  
+                          if(mysqli_num_rows($result) = 0)  
+                          {  echo("Таких записей нет");}
+                          else {
                                while($row = mysqli_fetch_array($result))  
                                {  
                           ?>  
@@ -48,17 +59,16 @@ else {
                             <td><?php echo $row["nation_name"]; ?></td>
                           </tr>  
                           <?php  
-                                 
                                }  
-                          }
-                            else { echo ("Такой записи в базе нет");}
+                          }  
                           ?>  
                      </table>  
                 </div>  
            </div>  
            <br /> 
-  <form method='post' action='alldata.php'><b/>
-<input id='submitread'  type='submit' value="На главную"><b/><b/>
+</body>
+<form method='post' action='alldata.php'><b/>
+<input id='submitread'  type='submit' value="Вернуться на главную"><b/><b/>
 </form>
 <form method="post" action="form.php">
 <input id="submitback" type="submit" value="К форме">
