0891e19057b5f89e92a419784e2614c1d2d0617b
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-04-22 00:15:47 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -21,7 +21,6 @@ $sql = "SELECT * FROM `spr_region`";
             $otch = $row['Ankety_otch'];
             $dob = $row['date_birth'];
             $id= $row['reg_id'];
-            $reg_name=$row['reg_name'];
             // Заполните остальные поля аналогичным образом
         }
     }
@@ -29,7 +28,7 @@ $sql = "SELECT * FROM `spr_region`";
     // Проверяем, была ли отправлена форма обновления данных
     if(isset($_POST['update'])) {
         // Получаем обновленные данные из формы
-        if(isset($_POST['q1'])&&isset($_POST['q2'])){
+        $ankety_id = mysqli_real_escape_string($con, $_POST['Ankety_id']);
       // Store the Ankety_fam in a "fam" variable
        $fam = mysqli_real_escape_string($con,$_POST['Ankety_fam']);
         // Store the Ankety_name1 in a "name" variable
@@ -40,44 +39,9 @@ $sql = "SELECT * FROM `spr_region`";
        $dob = date('Y-m-d', strtotime(($_POST['dateofbirth'])));
         // Store the reg_id in a "id" variable
         $id = mysqli_real_escape_string($con,$_POST['Region']); 
-        // Store the document in a "doc" variable
-       $doc = mysqli_real_escape_string($con,$_POST['document']);
-              // Store the nationality_id in a "id1" variable
-        $id1 = mysqli_real_escape_string($con,$_POST['Nationality']); 
-      // Store the country_id in a "id2" variable
-        $id2 = mysqli_real_escape_string($con,$_POST['Country']);
-      // Store the gender_id in a "id3" variable
-        $id3 = mysqli_real_escape_string($con,$_POST['Gender']);
-      // Store the vid_milit_serv_id in a "id4" variable
-        $id4 = mysqli_real_escape_string($con,$_POST['Milit_serve']);
-      // Store the 	document_mil in a "doc_mil" variable
-       $doc_mil = mysqli_real_escape_string($con,$_POST['document_mil']);
-      // Store the tel_home in a "tel1" variable
-       $tel1 = mysqli_real_escape_string($con,$_POST['Telhome']);
-      // Store the tel_mob in a "tel2" variable
-       $tel2 = mysqli_real_escape_string($con,$_POST['Telmob']);
-      // Store the uch_zav_id in a "id5" variable
-        $id5 = mysqli_real_escape_string($con,$_POST['Uch_zav']);
-      // Store the document_educ in a "doc_edc" variable
-       $doc_edc = mysqli_real_escape_string($con,$_POST['document_educ']);
-      // Store the trud_st in a "q1" variable
-        $q1=$_POST['q1'];
-      // Store the 	document_trud in a "doc_trd" variable
-       $doc_trd = mysqli_real_escape_string($con,$_POST['document_trud']);
-      // Store the language_id in a "id6" variable
-        $id6 = mysqli_real_escape_string($con,$_POST['Language']);
-      // Store the LIVING in a "q2" variable
-        $q2=$_POST['q2'];
-      // Store the 	vip_letter in a "vip" variable
-       $vip = mysqli_real_escape_string($con,$_POST['vip']);
-      // Store the mother in a "mother" variable
-       $mother = mysqli_real_escape_string($con,$_POST['Mother']);
-      // Store the 	father in a "	father" variable
-       $father= mysqli_real_escape_string($con,$_POST['Father']);
-      $sred=$_POST['sredbal'];
         // Запрос на обновление данных в базе данных
         $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',
-        `reg_id`='$id
+        `reg_id`='$id'
  WHERE `Ankety_id`='$ankety_id'";
         if(mysqli_query($con, $sql_update)) {
             echo '<script>alert("Data updated successfully")</script>';
@@ -114,7 +78,7 @@ $sql = "SELECT * FROM `spr_region`";
         <input type="text" name="Ankety_otch" value="<?php echo isset($otch) ? $otch : ''; ?>"required><br>
      <label>Дата рождения:</label>
       <input type="date" name="dateofbirth" value="<?php echo isset($dob) ? $dob : ''; ?>"required> <br>
-      <select name='Region'>
+      <select name="Region">
     <?php while($regions = mysqli_fetch_array($all_regions)):?>
     <option value="<?=$id?>"><?=$region['reg_name']</option>
     <?php endwhile?>
