f299a5edd3fafa311c039fd24344c256d17ee2f2
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-04-21 21:49:43 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -1,6 +1,21 @@
 <?php
     require_once 'conn.php';
-    
+    $sql = "SELECT * FROM `spr_region`";
+    $all_regions = mysqli_query($con,$sql);
+    $sql1 = "SELECT * FROM `nationality`";
+    $all_nationalities = mysqli_query($con,$sql1);
+   $sql2 = "SELECT * FROM `countries`";
+    $all_countries = mysqli_query($con,$sql2);
+     $sql3 = "SELECT * FROM `gender`";
+    $all_genders = mysqli_query($con,$sql3);
+$sql4 = "SELECT * FROM `vid_mili_serv`";
+    $all_vid_mili_services = mysqli_query($con,$sql4);
+ $sql5 = "SELECT * FROM `uch_zav`";
+    $all_uch_zaves = mysqli_query($con,$sql5);
+$sql6 = "SELECT * FROM `languages`";
+    $all_languages = mysqli_query($con,$sql6);
+$sql7 = "SELECT * FROM `languages`";
+    $all_languages = mysqli_query($con,$sql6);
     // Проверяем, была ли отправлена форма
     if(isset($_POST['submit'])) {
         // Получаем ID записи из формы
@@ -29,10 +44,14 @@
         $name = mysqli_real_escape_string($con, $_POST['Ankety_name1']);
                 $otch = mysqli_real_escape_string($con,$_POST['Ankety_otch']);
         $dob = date('Y-m-d', strtotime(($_POST['dateofbirth'])));
+                $id = mysqli_real_escape_string($con,$_POST['Region']); 
+
         // Получите остальные данные аналогичным образом
 
         // Запрос на обновление данных в базе данных
-        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch' WHERE `Ankety_id`='$ankety_id'";
+        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',
+        `reg_id`='$id
+ WHERE `Ankety_id`='$ankety_id'";
         if(mysqli_query($con, $sql_update)) {
             echo '<script>alert("Data updated successfully")</script>';
         } else {
@@ -68,6 +87,27 @@
         <input type="text" name="Ankety_otch" value="<?php echo isset($otch) ? $otch : ''; ?>"required><br>
      <label>Дата рождения:</label>
       <input type="date" name="dateofbirth" value="<?php echo isset($dob) ? $dob : ''; ?>"required> <br>
+        <label>Регион</label>
+     <select name="Region"><?php 
+                // use a while loop to fetch data 
+                // from the $all_categories variable 
+                // and individually display as an option
+                while ($regions = mysqli_fetch_array(
+                        $all_regions,MYSQLI_ASSOC)):; 
+            ?>
+                <option value="<?php echo $regions["id_region"];
+                    // The value we usually set is the primary key
+                ?>">
+                    <?php echo $regions["reg_name"];
+                        // To show the category name to the user
+                    ?>
+                </option>
+            <?php 
+                endwhile; 
+                // While loop must be terminated
+            ?>
+        </select>
+        <br>
         <input type="submit" value="Обновить" name="update">
     </form>
 </body>
