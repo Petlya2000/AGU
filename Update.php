31b3556d04727901d0b54280ac9b9e94cbac7322
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-04-21 21:43:18 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -27,10 +27,12 @@
         $ankety_id = mysqli_real_escape_string($con, $_POST['Ankety_id']);
         $fam = mysqli_real_escape_string($con, $_POST['Ankety_fam']);
         $name = mysqli_real_escape_string($con, $_POST['Ankety_name1']);
+                $otch = mysqli_real_escape_string($con,$_POST['Ankety_otch']);
+        $dob = date('Y-m-d', strtotime(($_POST['dateofbirth'])));
         // Получите остальные данные аналогичным образом
 
         // Запрос на обновление данных в базе данных
-        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name' WHERE `Ankety_id`='$ankety_id'";
+        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch' WHERE `Ankety_id`='$ankety_id'";
         if(mysqli_query($con, $sql_update)) {
             echo '<script>alert("Data updated successfully")</script>';
         } else {
@@ -50,9 +52,6 @@
     <form method="POST">
         <label>ID записи для обновления:</label>
         <input type="text" name="Ankety_id" required><br>
-        <label>Фамилия абитуриента:</label>
-        <input type="text" name="Ankety_fam" value="<?php echo isset($fam) ? $fam : ''; ?>" required><br>
-        <!-- Заполните остальные поля аналогично -->
         <input type="submit" value="Получить данные" name="submit">
     </form>
 
@@ -63,7 +62,12 @@
         <input type="text" name="Ankety_id" value="<?php echo isset($ankety_id) ? $ankety_id : ''; ?>" readonly><br>
         <label>Фамилия абитуриента:</label>
         <input type="text" name="Ankety_fam" value="<?php echo isset($fam) ? $fam : ''; ?>" required><br>
-        <!-- Заполните остальные поля аналогично -->
+    <label>Имя абитуриента:</label>
+        <input type="text" name="Ankety_name1" value="<?php echo isset($name) ? $name : ''; ?>"required><br>
+     <label>Отчество абитуриента:</label>
+        <input type="text" name="Ankety_otch" value="<?php echo isset($otch) ? $otch : ''; ?>"required><br>
+     <label>Дата рождения:</label>
+      <input type="date" name="dateofbirth" value="<?php echo isset($dob) ? $dob : ''; ?>"required> <br>
         <input type="submit" value="Обновить" name="update">
     </form>
 </body>
