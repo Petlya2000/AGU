d2d7aa7138c39972822035f6336b820e6ef4e587
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-04-29 13:59:05 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -8,12 +8,15 @@
         $ankety_id = mysqli_real_escape_string($con, $_POST['Ankety_id']);
 
         // Запрос к базе данных для получения информации о записи по ее ID
-        $sql_select = "SELECT * FROM `ankety` WHERE `Ankety_id`='$ankety_id'";
+        $sql_select = "SELECT * FROM ankety 
+               INNER JOIN spr_region ON ankety.reg_id = spr_region.id_region 
+               INNER JOIN gender ON ankety.gender_id = gender.gend_id 
+               INNER JOIN nationality ON ankety.nationality_id = nationality.nation_id 
+               WHERE `Ankety_id`='$ankety_id'";
         $result = mysqli_query($con, $sql_select);
         $row = mysqli_fetch_assoc($result);
-$sql = "SELECT * FROM `spr_region`";
-    $all_regions = mysqli_query($con,$sql);
-       
+       $sql= "SELECT * FROM 'spr_region'";
+        $all_regions= mysqli_query($con,$sql);
         // Если запись найдена, заполняем остальные поля формы
         if($row) {
             $fam = $row['Ankety_fam'];
@@ -21,12 +24,16 @@ $sql = "SELECT * FROM `spr_region`";
             $otch = $row['Ankety_otch'];
             $dob = $row['date_birth'];
             $id= $row['reg_id'];
+            $doc = $row['document'];
+            $q1=$row['trud_st'];
+            $sred=$row['srednbal'];
             // Заполните остальные поля аналогичным образом
         }
     }
 
     // Проверяем, была ли отправлена форма обновления данных
     if(isset($_POST['update'])) {
+        if(isset($_POST['q1'])){
         // Получаем обновленные данные из формы
         $ankety_id = mysqli_real_escape_string($con, $_POST['Ankety_id']);
       // Store the Ankety_fam in a "fam" variable
@@ -37,16 +44,20 @@ $sql = "SELECT * FROM `spr_region`";
         $otch = mysqli_real_escape_string($con,$_POST['Ankety_otch']);
         // Store the date_birth in a "dob" variable
        $dob = date('Y-m-d', strtotime(($_POST['dateofbirth'])));
+            // Store the trud_st in a "q1" variable
+        $q1=$_POST['q1'];
+         $sred=$_POST['sredbal'];
         // Store the reg_id in a "id" variable
         $id = mysqli_real_escape_string($con,$_POST['Region']); 
+        
         // Запрос на обновление данных в базе данных
-        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',
-        `reg_id`='$id'
+        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',`trud_st`='$q1',`srednbal`='$sred'
  WHERE `Ankety_id`='$ankety_id'";
         if(mysqli_query($con, $sql_update)) {
             echo '<script>alert("Data updated successfully")</script>';
         } else {
             echo '<script>alert("Error updating data")</script>';
+        }
         }
     }
 ?>
@@ -78,7 +89,13 @@ $sql = "SELECT * FROM `spr_region`";
         <input type="text" name="Ankety_otch" value="<?php echo isset($otch) ? $otch : ''; ?>"required><br>
      <label>Дата рождения:</label>
       <input type="date" name="dateofbirth" value="<?php echo isset($dob) ? $dob : ''; ?>"required> <br>
-      <select name="Region">
+      label>Трудовой стаж</label>
+       <br>
+     <input type="radio" name="q1" value="0"> Нет<br>
+     <input type="radio" name="q1" value="1"> Да<br>
+      <br>
+             <label>Регион:</label>
+        <select name="Region">
     <?php 
     while ($regions = mysqli_fetch_array(
                         $all_regions,MYSQLI_ASSOC)):; 
@@ -96,6 +113,9 @@ $sql = "SELECT * FROM `spr_region`";
             ?>
 </select>
         <br>
+        <label>Средний бал:</label>
+        <input type="number" name="sredbal" min="0" step="0.1" value="<?php echo isset($sred) ? $sred : ''; ?>"required>
+     <br>
         <br>
         <input type="submit" value="Обновить" name="update">
     </form>
