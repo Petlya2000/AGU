15055cc02100f44b1a675b93dae068667015af2d
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2024-05-13 15:35:32 +0300

    Update Update.php

--- Update.php
+++ Update.php
@@ -3,7 +3,23 @@
 	
 	$sql= "SELECT * FROM spr_region";
 	$all_regions= mysqli_query($con,$sql);
-   
+   // Get all the categories from category table
+    $sql = "SELECT * FROM `spr_region`";
+    $all_regions = mysqli_query($con,$sql);
+    $sql1 = "SELECT * FROM `nationality`";
+    $all_nationalities = mysqli_query($con,$sql1);
+   $sql2 = "SELECT * FROM `countries`";
+    $all_countries = mysqli_query($con,$sql2);
+     $sql3 = "SELECT * FROM `gender`";
+    $all_genders = mysqli_query($con,$sql3);
+$sql4 = "SELECT * FROM `vid_mili_serv`";
+    $all_vid_mili_services = mysqli_query($con,$sql4);
+ $sql5 = "SELECT * FROM `uch_zav`";
+    $all_uch_zaves = mysqli_query($con,$sql5);
+$sql6 = "SELECT * FROM `languages`";
+    $all_languages = mysqli_query($con,$sql6);
+$sql7 = "SELECT * FROM `languages`";
+    $all_languages = mysqli_query($con,$sql6);
     // Проверяем, была ли отправлена форма
     if(isset($_POST['submit'])) {
         // Получаем ID записи из формы
@@ -42,12 +58,7 @@
 
     // Проверяем, была ли отправлена форма обновления данных
     if(isset($_POST['update'])) {
-		
-		
-		
-        if(isset($_POST['q1'])){
-        // Получаем обновленные данные из формы
-        $ankety_id = mysqli_real_escape_string($con, $_POST['Ankety_id']);
+		if(isset($_POST['q1'])&&isset($_POST['q2'])){
       // Store the Ankety_fam in a "fam" variable
        $fam = mysqli_real_escape_string($con,$_POST['Ankety_fam']);
         // Store the Ankety_name1 in a "name" variable
@@ -56,15 +67,46 @@
         $otch = mysqli_real_escape_string($con,$_POST['Ankety_otch']);
         // Store the date_birth in a "dob" variable
        $dob = date('Y-m-d', strtotime(($_POST['dateofbirth'])));
-            // Store the trud_st in a "q1" variable
-        $q1=$_POST['q1'];
-                    $id = mysqli_real_escape_string($con,$_POST['Region']); 
-         $sred=$_POST['sredbal'];
         // Store the reg_id in a "id" variable
         $id = mysqli_real_escape_string($con,$_POST['Region']); 
+        // Store the document in a "doc" variable
+       $doc = mysqli_real_escape_string($con,$_POST['document']);
+              // Store the nationality_id in a "id1" variable
+        $id1 = mysqli_real_escape_string($con,$_POST['Nationality']); 
+      // Store the country_id in a "id2" variable
+        $id2 = mysqli_real_escape_string($con,$_POST['Country']);
+      // Store the gender_id in a "id3" variable
+        $id3 = mysqli_real_escape_string($con,$_POST['Gender']);
+      // Store the vid_milit_serv_id in a "id4" variable
+        $id4 = mysqli_real_escape_string($con,$_POST['Milit_serve']);
+      // Store the 	document_mil in a "doc_mil" variable
+       $doc_mil = mysqli_real_escape_string($con,$_POST['document_mil']);
+      // Store the tel_home in a "tel1" variable
+       $tel1 = mysqli_real_escape_string($con,$_POST['Telhome']);
+      // Store the tel_mob in a "tel2" variable
+       $tel2 = mysqli_real_escape_string($con,$_POST['Telmob']);
+      // Store the uch_zav_id in a "id5" variable
+        $id5 = mysqli_real_escape_string($con,$_POST['Uch_zav']);
+      // Store the document_educ in a "doc_edc" variable
+       $doc_edc = mysqli_real_escape_string($con,$_POST['document_educ']);
+      // Store the trud_st in a "q1" variable
+        $q1=$_POST['q1'];
+      // Store the 	document_trud in a "doc_trd" variable
+       $doc_trd = mysqli_real_escape_string($con,$_POST['document_trud']);
+      // Store the language_id in a "id6" variable
+        $id6 = mysqli_real_escape_string($con,$_POST['Language']);
+      // Store the LIVING in a "q2" variable
+        $q2=$_POST['q2'];
+      // Store the 	vip_letter in a "vip" variable
+       $vip = mysqli_real_escape_string($con,$_POST['vip']);
+      // Store the mother in a "mother" variable
+       $mother = mysqli_real_escape_string($con,$_POST['Mother']);
+      // Store the 	father in a "	father" variable
+       $father= mysqli_real_escape_string($con,$_POST['Father']);
+      $sred=$_POST['sredbal'];
         
         // Запрос на обновление данных в базе данных
-        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',`trud_st`='$q1',`reg_id`='$id',`srednbal`='$sred'
+        $sql_update = "UPDATE `ankety` SET `Ankety_fam`='$fam', `Ankety_name1`='$name', `Ankety_otch`='$otch', `date_birth`='$dob',`trud_st`='$q1',`reg_id`='$id',`document`='$doc',`srednbal`='$sred'
  WHERE `Ankety_id`='$ankety_id'";
   
         if(mysqli_query($con, $sql_update)) {
@@ -103,15 +145,9 @@
         <input type="text" name="Ankety_otch" value="<?php echo isset($otch) ? $otch : ''; ?>"required><br>
      <label>Дата рождения:</label>
       <input type="date" name="dateofbirth" value="<?php echo isset($dob) ? $dob : ''; ?>"required> <br>
-     <label>Трудовой стаж</label>
-       <br>
-     <input type="radio" name="q1" value="0"<?php echo (isset($q1) && $q1 == 0 ) ? ' checked' : '';?>> Нет<br>
-     <input type="radio" name="q1" value="1"<?php echo (isset($q1) && $q1 == 1 ) ? ' checked' : '';?>> Да<br>
-      <br>
-             <label>Регион:</label>
-        <select name="Region">
+     <label>Регион</label>
+     <select name="Region">
     <?php 
-	
     while ($regions = mysqli_fetch_array(
                         $all_regions,MYSQLI_ASSOC)):; 
             ?>
@@ -130,7 +166,10 @@
             ?>
 </select>
         <br>
-        <label>Средний бал:</label>
+      <label>Документ удостоверяющий личность:</label>
+        <input type="text" name="document" value="<?php echo isset($doc) ? $doc : ''; ?>" required><br>
+        <br>
+	    <label>Средний бал:</label>
         <input type="number" name="sredbal" min="0" step="0.1" value="<?php echo isset($sred) ? $sred : ''; ?>"required>
      <br>
         <br>
